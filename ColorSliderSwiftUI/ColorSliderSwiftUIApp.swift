458d7a3de25338aec991f20f70ce57eecdebe7cc
Evgeniy <ev.shipitsyn@yandex.ru>
2024-10-08 12:00:38 +0300

    Initial Commit

---
+++ ColorSliderSwiftUI/ColorSliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSliderSwiftUIApp.swift
+//  ColorSliderSwiftUI
+//
+//  Created by Евгений on 08.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorSliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
